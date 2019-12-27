7f0d4c3cd27af85895dda5d65ab66d9dfe891be1
RennanD <rplaykl@gmail.com>
2019-12-27 09:55:55 -0300

    Create global styles for web app

--- frontend/src/pages/Dashboard/index.js
+++ frontend/src/pages/Dashboard/index.js
@@ -3,5 +3,5 @@ import React from 'react';
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  return <div />;
+  return <h2>Dashboard</h2>;
 }
--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -1,19 +1,33 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 
+import AuthLayout from '../pages/_layouts/auth';
+import DefaultLayout from '../pages/_layouts/default';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate = false,
   ...rest
 }) {
-  const signed = false;
+  const signed = true;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
   }
   if (signed && !isPrivate) {
-    return <Component to="/dashboard" />;
+    return <Redirect to="/dashboard" />;
   }
 
-  return <Route {...rest} component={Component} />;
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
