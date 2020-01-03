aedfb805579211a96828a3721616e7ad27156ae0
RennanD <rplaykl@gmail.com>
2020-01-03 15:01:03 -0300

    Create feature to update profile in web app

--- frontend/src/store/modules/user/sagas.js
+++ frontend/src/store/modules/user/sagas.js
@@ -1,3 +1,32 @@
-import { all } from 'redux-saga/effects';
+import { all, takeLatest, put, call } from 'redux-saga/effects';
+import api from '~/services/api';
 
-export default all([]);
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+import { toast } from 'react-toastify';
+
+export function* updateProfile({ payload }) {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    try {
+        const profile = Object.assign(
+            {
+                name,
+                email,
+                avatar_id,
+            },
+            rest.oldPassword ? rest : {}
+        );
+
+        const response = yield call(api.put, '/users', profile);
+
+        toast.success('Dados alterados com sucesso!');
+
+        yield put(updateProfileSuccess(response.data));
+    } catch (err) {
+        toast.error('Error ao atualizar os perfil.');
+        yield put(updateProfileFailure());
+    }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
--- frontend/src/styles/global.js
+++ frontend/src/styles/global.js
@@ -18,11 +18,11 @@ export default createGlobalStyle`
     }
 
     html, body, #root {
-        height: 100%
+        height: 100%;
     }
 
     body {
-        -webkit-font-smoothing: antialiased !important
+        -webkit-font-smoothing: antialiased !important;
     }
 
     body, input, button {
