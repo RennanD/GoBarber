06f8f2faca0850422d17e6787b0026200884394c
RennanD <rplaykl@gmail.com>
2020-01-13 23:47:01 -0300

    Configure redux and redux saga in app mobile

---
+++ gobarber/src/store/modules/auth/sagas.js
@@ -0,0 +1,71 @@
+import { Alert } from 'react-native';
+import { all, takeLatest, call, put } from 'redux-saga/effects';
+
+import { singInSuccess, singFailure } from './actions';
+
+import api from '~/services/api';
+
+export function* singIn({ payload }) {
+    const { email, password } = payload;
+    try {
+        const response = yield call(api.post, 'sessions', {
+            email,
+            password,
+        });
+
+        const { token, user } = response.data;
+
+        if (user.provider) {
+            Alert.alert('Login failure', 'User cannot be a provider');
+            return;
+        }
+
+        yield put(singInSuccess(token, user));
+
+        // history.push('/dashboard');
+    } catch ({ response }) {
+        Alert.alert('Login failure', response.data.error);
+        yield put(singFailure());
+    }
+}
+
+export function setToken({ payload }) {
+    if (!payload) {
+        return;
+    }
+    const { token } = payload.auth;
+
+    if (token) {
+        api.defaults.headers.Authorization = `Bearer ${token}`;
+    }
+}
+
+export function* singUp({ payload }) {
+    const { name, email, password } = payload;
+
+    try {
+        yield call(api.post, '/users', {
+            name,
+            email,
+            password,
+            provider: true,
+        });
+
+        // history.push('/');
+    } catch ({ response }) {
+        Alert.alert('Register failure', response.data.error);
+
+        yield put(singFailure());
+    }
+}
+
+export function singOut() {
+    // history.push('/');
+}
+
+export default all([
+    takeLatest('persist/REHYDRATE', setToken),
+    takeLatest('@auth/SING_IN_REQUEST', singIn),
+    takeLatest('@auth/SING_UP_REQUEST', singUp),
+    takeLatest('@auth/SING_OUT', singOut),
+]);
---
+++ gobarber/src/store/modules/user/sagas.js
@@ -0,0 +1,30 @@
+import { Alert } from 'react-native';
+
+import { all, takeLatest, put, call } from 'redux-saga/effects';
+import api from '~/services/api';
+
+import { updateProfileSuccess, updateProfileFailure } from './actions';
+
+export function* updateProfile({ payload }) {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    try {
+        const profile = {
+            name,
+            email,
+            avatar_id,
+            ...(rest.oldPassword ? rest : {}),
+        };
+
+        const response = yield call(api.put, '/users', profile);
+
+        Alert.alert('Success!', 'Successfully data changed');
+
+        yield put(updateProfileSuccess(response.data));
+    } catch ({ response }) {
+        Alert.alert('Data change failure', response.data.error);
+        yield put(updateProfileFailure());
+    }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
