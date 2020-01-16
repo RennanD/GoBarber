1066ac7405a679ac63933279b8b8dbcf09836e97
RennanD <rplaykl@gmail.com>
2020-01-16 00:30:54 -0300

    Init appointment screens from mobile app

--- gobarber/src/routes.js
+++ gobarber/src/routes.js
@@ -1,7 +1,16 @@
+import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import SingIn from './screens/SingIn';
 import SingUp from './screens/SingUp';
+
+import SelectProvider from '~/screens/New/SelectProvider';
+import SelectDateTime from '~/screens/New/SelectDateTime';
+import Confirm from '~/screens/New/Confirm';
 
 import Dashboard from './screens/Dashboard';
 import Profile from './screens/Profile';
@@ -14,6 +23,35 @@ export default (signed = false) =>
                 App: createBottomTabNavigator(
                     {
                         Dashboard,
+                        New: {
+                            screen: createStackNavigator(
+                                {
+                                    SelectProvider,
+                                    SelectDateTime,
+                                    Confirm,
+                                },
+                                {
+                                    defaultNavigationOptions: {
+                                        headerTransparent: true,
+                                        headerTintColor: '#fff',
+                                        headerLeftContainerStyle: {
+                                            marginLeft: 20,
+                                        },
+                                    },
+                                }
+                            ),
+                            navigationOptions: {
+                                tabBarVisible: false,
+                                tabBarLabel: 'Agendar',
+                                tabBarIcon: ({ tintColor }) => (
+                                    <Icon
+                                        name="add-circle-outline"
+                                        size={20}
+                                        color={tintColor}
+                                    />
+                                ),
+                            },
+                        },
                         Profile,
                     },
                     {
