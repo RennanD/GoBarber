930ddf78fb98947610ac7a897f06ec2aec2f1f4f
RennanD <rplaykl@gmail.com>
2020-01-19 18:37:05 -0300

    Added datepiker in mobile app

--- gobarber/src/screens/New/SelectDateTime/index.js
+++ gobarber/src/screens/New/SelectDateTime/index.js
@@ -1,8 +1,29 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container } from './styles';
+
+import Background from '~/components/Background';
+import DateTimeInput from '~/components/DateTimeInput';
 
 export default function SelectDateTime() {
-    return <View />;
+    const [date, setDate] = useState(new Date());
+
+    return (
+        <Background>
+            <Container>
+                <DateTimeInput date={date} onChange={setDate} />
+            </Container>
+        </Background>
+    );
 }
+
+SelectDateTime.navigationOptions = ({ navigation }) => ({
+    title: 'Data do agendamento',
+    headerLeft: () => (
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Icon name="chevron-left" size={20} color="#fff" />
+        </TouchableOpacity>
+    ),
+});
